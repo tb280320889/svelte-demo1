637a21b8a5749ad076bfe10d93f425218f7a9a4c
Sherlock Tang <280320889@qq.com>
2025-04-16 17:02:41 +0800

    📸 Feat: finish class 1
    class 1 content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,28 @@
 <script lang="ts">
+	function onclick(){
+		console.log('button clicked ');
+	}
+
+	let number = 5
+
 </script>
 
 <h1>Welcome to Svelte 5</h1>
+<p>{number}</p>
+
+<button {onclick}>
+	Click me!
+</button>
+<div class="container">
+	<p>p1</p>
+	<p>p2</p>
+</div>
+
+<style>
+	h1 {
+		color: aqua
+	}
+	.container {
+		background-color: white;
+	}
+</style>
